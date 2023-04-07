204b0c728dbd5b9eefff98d2896ae336eede60a7
Reza <99068159@mydavinci.nl>
2023-04-07 05:28:49 +0200

    Heb formulier van Onkunde gemaakt. Moet nog het antwoord maken. Heb ook de h1 verandert naar een h2 omdat er later nog een h1 boven aan de pagina gaat komen met de titel 'Mad Libs'

--- Er-heerst-paniek.php
+++ Er-heerst-paniek.php
@@ -32,14 +32,14 @@ function test_input($data) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="MadLibs.css">
-    <title>Document</title>
+    <title>Er heerst paniek...</title>
 </head>
 
     <body>
     
     <div class="container">
 
-    <h1>Er heerst paniek...</h1>
+    <h2>Er heerst paniek...</h2>
 
     <?php  if ($_SERVER['REQUEST_METHOD'] == "GET" || !empty($error)) { ?>
             
---
+++ Onkunde.php
@@ -0,0 +1,142 @@
+<?php
+    $error = [];
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+
+        foreach($_POST as $key => $value){
+            if (!$value) {
+                $error[$key] = "$key is niet ingevuld";
+            } else {
+                $_POST[$key] = test_input($value);
+            }
+        };
+    } else {
+    };
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MadLibs.css">
+    <title>Onkunde</title>
+</head>
+
+    <body>
+    
+    <div class="container">
+
+    <h2>Onkunde</h2>
+
+    <?php  if ($_SERVER['REQUEST_METHOD'] == "GET" || !empty($error)) { ?>
+            
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+        <div class="vraag">
+                <label for="willen-kunnen">Wat zou je graag willen kunnen? </label>
+                <input type="text" name="willen-kunnen" id="willen-kunnen" value="<?php echo isset($_POST['willen-kunnen']) ? $_POST['willen-kunnen'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("willen-kunnen", $error)){ ?>
+                    <br>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+
+        <div class="vraag">
+                <label for="opschieten">Met welke persoon kun je goed opschieten? </label>
+                <input type="text" name="opschieten" id="opschieten" value="<?php echo isset($_POST['opschieten']) ? $_POST['opschieten'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("opschieten", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+
+        <div class="vraag">
+                <label for="getal">Wat is je favoriete getal? </label>
+                <input type="text" name="getal" id="getal" value="<?php echo isset($_POST['getal']) ? $_POST['getal'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("getal", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+
+        <div class="vraag">
+                <label for="vakantie-item">Wat heb je altijd bij je als je op vakantie gaat? </label>
+                <input type="text" name="vakantie-item" id="vakantie-item" value="<?php echo isset($_POST['vakantie-item']) ? $_POST['vakantie-item'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("vakantie-item", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+
+        <div class="vraag">
+                <label for="beste-eigenschap">Wat is je beste persoonlijke eigenschap? </label>
+                <input type="text" name="beste-eigenschap" id="beste-eigenschap" value="<?php echo isset($_POST['beste-eigenschap']) ? $_POST['beste-eigenschap'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("beste-eigenschap", $error)){ ?>
+                    <br>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+
+        <div class="vraag">
+                <label for="slechtste-eigenschap">Wat is je slechtste persoonlijke eigenschap? </label>
+                <input type="text" name="slechtste-eigenschap" id="slechtste-eigenschap" value="<?php echo isset($_POST['slechtste-eigenschap']) ? $_POST['slechtste-eigenschap'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("slechtste-eigenschap", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+
+        <div class="vraag">
+                <label for="overkomen">Wat is het ergste dat je kan overkomen? </label>
+                <input type="text" name="overkomen" id="overkomen" value="<?php echo isset($_POST['overkomen']) ? $_POST['overkomen'] : '' ?>">
+                <!-- <span style="color:red">*</span> -->
+                <br>
+                <?php if(array_key_exists("overkomen", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
+        </div>
+        
+        <div class="submit-button"  >
+        <input id="submit" type="submit" value="Versturen">
+        </div>
+        </form>
+
+        <?php }  else { ?>            
+
+            <p>"<?php echo($_POST['willen-kunnen'])?>."</p>
+
+            
+        <?php } ?> 
+
+        </div>
+        
+    </body>
+</html>
